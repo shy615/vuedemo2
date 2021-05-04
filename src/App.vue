ef9a717ff5666a52a572f6e121cedffdcb613dd1
shy615 <13862636243@163.com>
2021-05-04 15:22:07 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+
+    <el-container>
+      <el-header>Header</el-header>
+      <el-container>
+        <el-aside width="200px">
+          <navmenu></navmenu>
+        </el-aside>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+
+  </div>
+</template>
+
+<script>
+import NavMenu from '@/components/NavMenu'
+
+export default {
+  name: 'app',
+  components: {
+    'navmenu': NavMenu
+  }
+}
+
+</script>
+
+<style>
+
+</style>
